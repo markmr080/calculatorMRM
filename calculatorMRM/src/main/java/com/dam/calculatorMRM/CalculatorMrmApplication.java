39be050500d09926bee77d4b3e6bcc6c6fe04fcd
markmr080 <markmr080@gmail.com>
2026-04-23 19:46:38 +0200

    Añadido esqueleto Spring Boot

---
+++ calculatorMRM/src/main/java/com/dam/calculatorMRM/CalculatorMrmApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMrmApplication.class, args);
+	}
+
+}
---
+++ calculatorMRM/src/test/java/com/dam/calculatorMRM/CalculatorMrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorMrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
