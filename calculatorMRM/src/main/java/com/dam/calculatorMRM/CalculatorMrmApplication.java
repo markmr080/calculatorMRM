5fb86caf62591f76d82bff50d51d1af0c64931f7
markmr080 <markmr080@gmail.com>
2026-05-04 21:25:28 +0200

    Prueba ChecksStyle

--- calculatorMRM/src/main/java/com/dam/calculatorMRM/CalculatorMrmApplication.java
+++ calculatorMRM/src/main/java/com/dam/calculatorMRM/CalculatorMrmApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorMrmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorMrmApplication.class, args);
+public static void main(String[] args) {
+SpringApplication.run(CalculatorMrmApplication.class, args);
 
-	}
+}
 
 }
